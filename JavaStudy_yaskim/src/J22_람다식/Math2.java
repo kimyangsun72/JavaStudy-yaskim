b7eeee9d70c63f1d7ace1b21b614509105613b6e
DESKTOP-72VS647\KR <yaskim@krs.co.kr>
2022-06-29 14:46:11 +0900

    no message

--- JavaStudy_yaskim/src/J22_람다식/Math2.java
+++ JavaStudy_yaskim/src/J22_람다식/Math2.java
@@ -1,6 +1,8 @@
 package J22_람다식;
 
 @FunctionalInterface
-public interface Math2 {
+
+public interface Math2 {   // imlements한 것은 interface해야 됨
 	public double calc(double value1, double value2);
+
 }
